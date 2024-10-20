36c1fb9010e863e5c44f1a7302371da62483e964
LeoGR22 <leonardogr2212@gmail.com>
2024-10-20 11:31:17 -0300

    Fix Node Bugs

--- Assets/Scripts/Match3/Node.cs
+++ Assets/Scripts/Match3/Node.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour
+public class Node
 {
-   public bool isUsable;
-   public GameObject potion;
+    public bool isUsable;
+    public GameObject potion;
 
-   public Node(bool _isUsable, GameObject _potion)
-   {
-        isUsable = _isUsable;
-        potion = _potion;
-   }
+    public Node(bool usable, GameObject potion)
+    {
+        isUsable = usable;
+        this.potion = potion;
+    }
 }
